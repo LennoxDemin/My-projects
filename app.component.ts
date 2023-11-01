1b9bfd16528e5171abc26344565ff3baab2ef0ea
muthiani musembi <123635111+LennoxDemin@users.noreply.github.com>
2023-11-01 11:01:33 +0300

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-first-project';
+  name = 'musembis markdocs';
+  notes = 'Markdown is a simple and easy-to-use mark up language that can be used to format virtually any document. It is widely used in blogging, instant messaging, online forums, collaborative software, documentation pages, and readme files'
+}
